9dd4f9bdaa651f45e6243afd38430f428f8c0e53
Jason Kiraly <jaykiraly@gmail.com>
2015-10-07 21:33:32 +0000

    App done, pre refactor.

--- application.js
+++ application.js
@@ -5,7 +5,7 @@ $(function() {
       var url, movie, data;
       e.preventDefault();
   movie = form.find('input').val();
-      url = 'http://www.omdbapi.com/?t=' + movie + '&type=movie&tomatoes=true'
+      url = 'http://www.omdbapi.com/?t=' + movie + '&tomatoes=true'
       $.ajax(url, {
           complete: function(xhr, status){
               data = $.parseJSON(xhr.responseText);
@@ -14,6 +14,11 @@ $(function() {
               container.find('.plot').text(data.Plot);
               container.find('.poster').html('<img src="' + data.Poster + '"/>');
               container.find('.year').text(data.Year);
+              container.find('.imdb-rating').text(data.imdbRating);
+              container.find('.imdb-votes').text(data.imdbVotes);
+              container.find('.tomato-rating').text(data.tomatoRating);
+              container.find('.tomato-votes').text(data.tomatoReviews);
+              container.find('.tomato-image').html('<img src="http://d3biamo577v4eu.cloudfront.net/static/images/logos/rtlogo.png"/>');
               container.show();
           }
       });    
