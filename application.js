369c9aa159d13ab3bed6f5c834806f9deba7da0c
Jason Kiraly <jaykiraly@gmail.com>
2015-10-08 01:21:42 +0000

    Multi search working.

--- application.js
+++ application.js
@@ -1,10 +1,11 @@
 $(function() {
 
-  var form = $('form'), container = $('#container');
-  form.on('submit', function(e){
+  var formSingle = $('#single-search');
+  var formMulti = $('#multi-search');
+  formSingle.on('submit', function(e){
     var url, movie, data;
     e.preventDefault();
-  movie = form.find('input').val();
+  movie = formSingle.find('input').val();
       url = 'http://www.omdbapi.com/?t=' + movie + '&type=movie&tomatoes=true'
       $.ajax(url, {
           complete: function(xhr, status){
@@ -17,5 +18,21 @@ $(function() {
           }
       });     
   });
-
+  formMulti.on('submit', function(e){
+    var url, movie, data;
+    e.preventDefault();
+    movie = formMulti.find('input').val();
+    url = 'http://www.omdbapi.com/?s=' + movie + '&type=movie&tomatoes=true'
+    $.getJSON(url, function(data) {
+      console.log(data);
+      $.each(data['Search'], function(index, value) {
+        console.log(value);
+        console.log(index);
+        var templateScript = $("#movie-template").html();
+        var template = Handlebars.compile(templateScript);
+        var compiledHtml = template(value);
+        $('#place-content').append(compiledHtml);  
+      });
+    });
+  });    
 });
